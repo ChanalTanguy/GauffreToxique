b502f03eb50da105dc125b0273613d5ca8daca80
vov <vov@127.0.0.1>
2015-05-11 14:55:04 +0200

    update Constantes.java

--- GauffreToxique/src/Constantes.java
+++ GauffreToxique/src/Constantes.java
@@ -2,7 +2,7 @@
 public class Constantes {
 	public static final String clicInvalide = "clic invalide";
 	public static final String partieTerminee = "Partie terminée, joueur perdant : ";
-	
+	 
 	//Constantes graphiques
 	public static final int tailleCase = 50;
 	
