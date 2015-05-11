91b291d5a39a177168ab4ef783788f8822028e92
Piapiou <pierre-42@live.fr>
2015-05-11 13:30:20 +0200

    Ajoue interface d'entrée, et de la classe de jeu.

---
+++ GauffreToxique/src/Jeu.java
@@ -0,0 +1,28 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Jeu {
+	
+	Plateau gauffre;
+	int joueur;
+	ArrayList<Plateau> plateauPrec;
+	ArrayList<Plateau> plateauSuiv;
+	
+	void init() {}
+	void boucle() {}
+	
+	void save() {}
+	void load() {}
+	
+	void undo() {}
+	void redo() {}
+	
+	private Point recupClick() {
+		return null;
+	}
+	
+	private boolean partieFinie() {
+		return false;
+	}
+
+}
