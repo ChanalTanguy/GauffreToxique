e7cc5b6900f6427715218b97b8f5f7fbd8b1d629
deathnight <deathnight@deathnight-N150P>
2015-05-12 11:26:46 +0200

    configuration.java : item de l'inventaire

--- GauffreToxique/src/Configuration.java
+++ GauffreToxique/src/Configuration.java
@@ -5,18 +5,22 @@ public class Configuration {
 int joueur_courant;
 Plateau plat;
 
+//on charge le plateau depuis la configuration
 Plateau Charger_Plateau(){
 	return plat; 
 }
 
+//on charge le joueur courant depuis la configuration
 int Charger_Joueur(){
 	return joueur_courant;
 }
 
+//on sauvegarde le plateau dans la configuration
 void Sauvegarder_Plateau(Plateau p){
 	plat=p.clone();
 }
 
+//on sauvegarde le joueur courant dans la configuration
 void Sauvegarder_Joueur(int i){
 	joueur_courant=i;
 }
