542d5d7e3c114d6e4496507bfeb627107a2d44ae
deathnight <deathnight@deathnight-N150P>
2015-05-12 11:33:13 +0200

    update Configuration

--- GauffreToxique/src/Configuration.java
+++ GauffreToxique/src/Configuration.java
@@ -5,23 +5,35 @@ public class Configuration {
 int joueur_courant;
 Plateau plat;
 
+//constructeur d'une configuration vide
+public Configuration(){
+	Plateau p = new Plateau(0,0);
+	joueur_courant = 0;
+	plat=p.clone();
+}
+
+public Configuration(int joueur, Plateau p){
+	joueur_courant = joueur;
+	plat=p.clone();
+}
+
 //on charge le plateau depuis la configuration
-Plateau Charger_Plateau(){
+protected Plateau Charger_Plateau(){
 	return plat; 
 }
 
 //on charge le joueur courant depuis la configuration
-int Charger_Joueur(){
+protected int Charger_Joueur(){
 	return joueur_courant;
 }
 
 //on sauvegarde le plateau dans la configuration
-void Sauvegarder_Plateau(Plateau p){
+protected void Sauvegarder_Plateau(Plateau p){
 	plat=p.clone();
 }
 
 //on sauvegarde le joueur courant dans la configuration
-void Sauvegarder_Joueur(int i){
+protected void Sauvegarder_Joueur(int i){
 	joueur_courant=i;
 }
 }
