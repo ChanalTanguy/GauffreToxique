95df0a998ea49b5484304ae5ee7ce8072467baa2
Piapiou <pierre-42@live.fr>
2015-05-12 10:51:54 +0200

    Ajustement IAMoyenne

--- GauffreToxique/src/IAMoyenne.java
+++ GauffreToxique/src/IAMoyenne.java
@@ -13,11 +13,12 @@ public class IAMoyenne implements InterfaceEntree {
 		
 		// Vérifier qu'il y a un coup gagnant
 		boolean [][] g = gauffre.getTab();
+		if (!g[0][1] && !g[1][0])
+			return new Point(0,0);
 		if (!g[0][1])
 			return new Point(1,0);
 		if (!g[1][0])
 			return new Point(0,1);
-		
 		// Générer une position valide qui ne perd pas (donne une position qui perd après 5 essai)
 		Random r = new Random();
 		int i = 0;
@@ -27,13 +28,13 @@ public class IAMoyenne implements InterfaceEntree {
 			do {
 				p = new Point(r.nextInt(gauffre.getWidth()),r.nextInt(gauffre.getHeight())); 
 			} while (!g[p.x][p.y]);
-		} while (i < 5 && !coupPerdant(p));
+		} while (i < 25 && coupPerdant(p));
 		
 		return p;
 	}
 
 	private boolean coupPerdant(Point p) {
-		return (p.x == 0 && p.y == 1 || p.x == 1 && p.y == 0);
+		return ((p.x == 0 && p.y == 1) || (p.x == 1 && p.y == 0) || (p.x == 0 && p.y == 0));
 	}
 	
 }
