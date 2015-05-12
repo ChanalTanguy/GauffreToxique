2dcceefdd05db0fed9cdcb13c3960d37a84a7f1d
vov <vov@127.0.0.1>
2015-05-12 15:53:57 +0200

    petite MaJ de merde (position de la fenetre a l'apparition)

--- GauffreToxique/src/mainPackage/Graphique.java
+++ GauffreToxique/src/mainPackage/Graphique.java
@@ -20,46 +20,48 @@ public class Graphique {
         // On fixe la taille et on demarre
         frame.setSize(gaufre.getWidth()*Constantes.tailleCase+200, gaufre.getHeight()*Constantes.tailleCase+25+25+100);
         frame.setJMenuBar(bar);
+        frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
     }
 	
 	//Cas 1 humain + 1 IA
 	public Graphique(Plateau gaufre, InterfaceEntree IA) {
-        Dessin d = new Dessin(gaufre, IA);
-        
-        //Test mouse
-        d.addMouseListener(new EcouteTerrain(d, gaufre));
-        frame.addKeyListener(new Konami(d,gaufre));
-        
-        frame.add(d);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // On fixe la taille et on demarre
-        frame.setSize(gaufre.getWidth()*Constantes.tailleCase+200, gaufre.getHeight()*Constantes.tailleCase+25+25+100);
-        frame.setJMenuBar(bar);
-        frame.setResizable(false);
-        frame.setVisible(true);
-    }
+		Dessin d = new Dessin(gaufre, IA);
+		//Test mouse
+		d.addMouseListener(new EcouteTerrain(d, gaufre));
+		frame.addKeyListener(new Konami(d,gaufre));
+		
+		frame.add(d);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// On fixe la taille et on demarre
+		frame.setSize(gaufre.getWidth()*Constantes.tailleCase+200, gaufre.getHeight()*Constantes.tailleCase+25+25+100);
+		frame.setJMenuBar(bar);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 
 	//Cas 2 IA
 	public Graphique(Plateau gaufre, InterfaceEntree IA1, InterfaceEntree IA2)
 	{
 		 
-	     Dessin d = new Dessin(gaufre, IA1, IA2);
-	        
-	     //Test mouse
-	     d.addMouseListener(new EcouteTerrain(d, gaufre));
-	     frame.addKeyListener(new Konami(d,gaufre));
-	        
-	     frame.add(d);
-	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        
-	     // On fixe la taille et on demarre
-	     frame.setSize(gaufre.getWidth()*Constantes.tailleCase+200, gaufre.getHeight()*Constantes.tailleCase+25+25+100);
-	     frame.setJMenuBar(bar);
-	     frame.setResizable(false);
-	     frame.setVisible(true);
+		Dessin d = new Dessin(gaufre, IA1, IA2);
+		
+		//Test mouse
+		d.addMouseListener(new EcouteTerrain(d, gaufre));
+		frame.addKeyListener(new Konami(d,gaufre));
+		
+		frame.add(d);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// On fixe la taille et on demarre
+		frame.setSize(gaufre.getWidth()*Constantes.tailleCase+200, gaufre.getHeight()*Constantes.tailleCase+25+25+100);
+		frame.setJMenuBar(bar);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 	
 }
